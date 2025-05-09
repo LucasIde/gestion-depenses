c5fce9220900ad218eccbc0d64e5d0f4724cc93d
lucas ide <lucaside000@gmail.com>
2025-05-09 16:02:58 +0200

    bonus all done

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,15 @@ const btn = document.querySelector(".add");
 const resume = document.querySelector(".résumer");
 const tot = document.querySelector(".total");
 const date = document.querySelector(".date");
+const alim_box = document.querySelector(".alimentation_box");
+const logement_box = document.querySelector(".logement_box");
+const transport_box = document.querySelector(".transport_box");
+const div_box = document.querySelector(".divertissement_box");
+const empty = document.querySelector(".empty");
+const tot_alim = document.querySelector(".tot_alim");
+const tot_log = document.querySelector(".tot_log");
+const tot_tr = document.querySelector(".tot_tr");
+const tot_dive = document.querySelector(".tot_dive");
 
 // ==============================
 // 🌍 Variables globales
@@ -45,36 +54,93 @@ function create_tab() {
 
 // add element in html
 
-function add_resume(txt, index) {
+function append_categorie(div, key) {
+	switch (key) {
+		case "alimentation": {
+			alim_box.appendChild(div);
+			break;
+		}
+		case "logement": {
+			logement_box.appendChild(div);
+			break;
+		}
+		case "transport": {
+			transport_box.appendChild(div);
+			break;
+		}
+		case "divertissement": {
+			div_box.appendChild(div);
+			break;
+		}
+	}
+}
+
+function add_resume(txt, index, key) {
 	let div = document.createElement("div");
 	div.innerHTML = `${txt} <button class="delete">❌</button>`;
 	div.setAttribute("data-index", index);
-	resume.appendChild(div);
+	append_categorie(div, key);
+	// resume.appendChild(div);
 };
 
 // delete all + add all remaining tab element
 
+function delete_box() {
+	alim_box.innerHTML = "";
+	logement_box.innerHTML = "";
+	transport_box.innerHTML = "";
+	div_box.innerHTML = "";
+}
+
 function reorganize_index() {
-	resume.innerHTML = "";
+	delete_box();
 	let i = 0;
 	tab.forEach((x) => {
 		const emoji = find_emoji(x[0])
-		add_resume(`${emoji} - ${x[1]} - ${x[2]}€ - ${x[3]}`, i++);
+		add_resume(`${emoji} - ${x[1]} - ${x[2]}€ - ${x[3]}`, i++ , x[0]);
 	});
 };
 
+// add total
+
 function add_tot() {
+	let total_alim = 0;
+	let total_log = 0;
+	let total_tra = 0;
+	let total_div = 0;
 	let total = 0;
 	if (tab.length != 0) {
 		tab.forEach((x) => {
-			total += (Number)(x[2]);
+			switch (x[0]) {
+				case "alimentation": {
+					total_alim += (Number)(x[2]);
+					break;
+				}
+				case "logement": {
+					total_log += (Number)(x[2]);
+					break;
+				}
+				case "transport": {
+					total_tra += (Number)(x[2]);
+					break;
+				}
+				case "divertissement": {
+					total_div += (Number)(x[2]);
+					break;
+				}
+			}
 		});
 	}
+	tot_alim.innerHTML = total_alim;
+	tot_log.innerHTML = total_log;
+	tot_tr.innerHTML = total_tra;
+	tot_dive.innerHTML = total_div;
+	total = total_alim + total_log + total_tra + total_div;
 	tot.innerHTML = `Total: ${total} €`;
 };
 
 function printEmpty() {
-	resume.innerHTML = "Aucune dépense enregistrée.";
+	empty.innerHTML = "Aucune dépense enregistrée.";
 };
 
 function clear_form() {
@@ -97,10 +163,10 @@ btn.addEventListener("click", function(e) {
 	e.preventDefault();
 	if (description.value && amount.value && rubric.value && date.value && amount.value >= 0) {
 		if (tab.length == 0) {
-			resume.innerHTML = '';
+			empty.innerHTML = '';
 		}
 		let txt_resume = create_tab();
-		add_resume(txt_resume, (tab.length - 1));
+		add_resume(txt_resume, (tab.length - 1), rubric.value);
 		add_tot();
 		console.log(tab);
 	}
@@ -120,6 +186,7 @@ resume.addEventListener("click", function(e) {
 			reorganize_index();
 		}
 		else {
+			delete_box();
 			printEmpty();
 		}
 		add_tot();
